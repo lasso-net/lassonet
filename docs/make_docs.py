1a25a25c977e0f8b7b70fc84cd7dcf312eac51c8
Louis Abraham <louis.abraham@yahoo.fr>
2021-03-10 22:39:16 +0100

    docs: initial version

---
+++ docs/make_docs.py
@@ -0,0 +1,20 @@
+import os
+import re
+
+import markdown2
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+with open("template.html") as f:
+    template = f.read()
+
+
+def replace(match):
+    filename = match.group(1) + ".md"
+    return markdown2.markdown_path(filename)
+
+
+index = re.sub("{{(\w+)}}", replace, template)
+
+with open("index.html", "w") as f:
+    f.write(index)
