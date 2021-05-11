35946413caaad0c8e1f62af98d72eb4a16f2603c
Louis Abraham <louis.abraham@yahoo.fr>
2021-05-11 16:43:53 +0200

    add new functions to imports

--- lassonet/__init__.py
+++ lassonet/__init__.py
@@ -1,3 +1,4 @@
 from .model import LassoNet
 from .prox import prox
-from .interfaces import LassoNetClassifier, LassoNetRegressor
+from .interfaces import LassoNetClassifier, LassoNetRegressor, lassonet_path
+from .utils import plot_path
