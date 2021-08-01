06b8152781878ec64ab4a6a0e6b91ba1de7f27db
Louis Abraham <louis.abraham@yahoo.fr>
2021-08-01 16:42:17 +0200

    add custom score option to plot_path

--- lassonet/utils.py
+++ lassonet/utils.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
+from functools import partial
 
 
-def plot_path(model, path, X_test, y_test):
+def plot_path(model, path, X_test, y_test, *, score_function=None):
     """
     Plot the evolution of the model on the path, namely:
     - lambda
@@ -16,17 +17,28 @@ def plot_path(model, path, X_test, y_test):
         output of model.path
     X_test : array-like
     y_test : array-like
+    score_function : function or None
+        if None, use score_function=model.score
+        score_function must take as input X_test, y_test
     """
+    if score_function is None:
+        score_fun = model.score
+    else:
+        assert callable(score_function)
+
+        def score_fun(X_test, y_test):
+            return score_function(y_test, model.predict(X_test))
+
     n_selected = []
     score = []
     lambda_ = []
     for save in path:
         model.load(save.state_dict)
         n_selected.append(save.selected.sum())
-        score.append(model.score(X_test, y_test))
+        score.append(score_fun(X_test, y_test))
         lambda_.append(save.lambda_)
 
-    plt.figure(figsize=(12, 12))
+    plt.figure(figsize=(8, 8))
 
     plt.subplot(311)
     plt.grid(True)
