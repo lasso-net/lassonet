31a8f193b2c268986a84d55198510382baf129ad
Louis Abraham <louis.abraham@yahoo.fr>
2021-12-29 15:53:12 +0000

    add examples

---
+++ examples/generated.py
@@ -0,0 +1,71 @@
+from functools import partial
+
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+
+
+from lassonet import LassoNetRegressor, plot_path
+
+
+def linear():
+    p = 10
+    n = 400
+    coef = np.concatenate([np.random.choice([-1, 1], size=p), [0] * p])
+    X = np.random.randn(n, 2 * p)
+
+    linear = X.dot(coef)
+    noise = np.random.randn(n)
+
+    y = linear + noise
+    return X, y
+
+
+def strong_linear():
+    p = 10
+    n = 400
+    coef = np.concatenate([np.random.choice([-1, 1], size=p), [0] * p])
+    X = np.random.randn(n, 2 * p)
+
+    linear = X.dot(coef)
+    noise = np.random.randn(n)
+    x1, x2, x3, *_ = X.T
+    nonlinear = 2 * (x1 ** 3 - 3 * x1) + 4 * (x2 ** 2 * x3 - x3)
+    y = 6 * linear + 8 * noise + nonlinear
+    return X, y
+
+
+def friedman_lockout():
+    p = 200
+    n = 1000
+    X = np.random.rand(n, p)
+    y = (
+        10 * np.sin(np.pi * X[:, 0] * X[:, 1])
+        + 20 * (X[:, 2] - 0.5) ** 2
+        + 10 * X[:, 3]
+        + 5 * X[:, 4]
+    )
+    return X, y
+
+
+for generator in [linear, strong_linear, friedman_lockout]:
+    X, y = generator()
+    X = StandardScaler().fit_transform(X)
+    y -= y.mean()
+    y /= y.std()
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    model = LassoNetRegressor(verbose=True, path_multiplier=1.01, hidden_dims=(10, 10))
+
+    path = model.path(X_train, y_train)
+    import matplotlib.pyplot as plt
+
+    def score(self, X, y, sample_weight=None):
+        y_pred = self.predict(X)
+        return np.sqrt(1 - r2_score(y, y_pred, sample_weight=sample_weight))
+
+    model.score = partial(score, model)
+
+    plot_path(model, path, X_test, y_test)
+    plt.show()
---
+++ examples/mnist_ae.py
@@ -0,0 +1,84 @@
+from sklearn.datasets import fetch_openml
+from lassonet import LassoNetAutoEncoder
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
+X, y = fetch_openml(name="mnist_784", return_X_y=True)
+filter = y == "3"
+X = X[filter].values / 255
+
+model = LassoNetAutoEncoder(
+    M=30, n_iters=(3000, 500), path_multiplier=1.05, verbose=True
+)
+path = model.path(X)
+
+img = model.feature_importances_.reshape(28, 28)
+
+plt.title("Feature importance to reconstruct 3")
+plt.imshow(img)
+plt.colorbar()
+plt.savefig("mnist-ae-importance.png")
+
+
+n_selected = []
+score = []
+lambda_ = []
+
+for save in path:
+    model.load(save.state_dict)
+    X_pred = model.predict(X)
+    n_selected.append(save.selected.sum())
+    score.append(mean_squared_error(X_pred, X))
+    lambda_.append(save.lambda_)
+
+to_plot = [160, 220, 300]
+
+for i, save in zip(n_selected, path):
+    if not to_plot:
+        break
+    if i > to_plot[-1]:
+        continue
+    to_plot.pop()
+    plt.clf()
+    plt.title(f"Linear model with {i} features")
+    weight = save.state_dict["skip.weight"]
+    img = (weight[1] - weight[0]).reshape(28, 28)
+    plt.imshow(img)
+    plt.colorbar()
+    plt.savefig(f"mnist-ae-{i}.png")
+
+plt.clf()
+
+fig = plt.figure(figsize=(12, 12))
+
+plt.subplot(311)
+plt.grid(True)
+plt.plot(n_selected, score, ".-")
+plt.xlabel("number of selected features")
+plt.ylabel("MSE")
+
+plt.subplot(312)
+plt.grid(True)
+plt.plot(lambda_, score, ".-")
+plt.xlabel("lambda")
+plt.xscale("log")
+plt.ylabel("MSE")
+
+plt.subplot(313)
+plt.grid(True)
+plt.plot(lambda_, n_selected, ".-")
+plt.xlabel("lambda")
+plt.xscale("log")
+plt.ylabel("number of selected features")
+
+plt.savefig("mnist-ae-training.png")
+
+
+plt.subplot(221)
+plt.imshow(X[150].reshape(28, 28))
+plt.subplot(222)
+plt.imshow(model.predict(X[150]).reshape(28, 28))
+plt.subplot(223)
+plt.imshow(X[250].reshape(28, 28))
+plt.subplot(224)
+plt.imshow(model.predict(X[250]).reshape(28, 28))
