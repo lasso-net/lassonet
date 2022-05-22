8be3819902bee14e80f460e14aa8520eb42a72e6
Louis Abraham <louis.abraham@yahoo.fr>
2022-05-22 16:58:50 +0200

    fix bug

--- lassonet/interfaces.py
+++ lassonet/interfaces.py
@@ -382,7 +382,7 @@ class BaseLassoNet(BaseEstimator, metaclass=ABCMeta):
                 X, y, test_size=self.val_size, random_state=self.random_state
             )
         else:
-            X_val, y_val = X, y
+            X_train, y_train = X_val, y_val = X, y
         X_train, y_train = self._cast_input(X_train, y_train)
         X_val, y_val = self._cast_input(X_val, y_val)
 
