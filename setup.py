ac50fae68e932a3ec11fce010148c21a3e54c8ff
Louis Abraham <louisabraham@users.noreply.github.com>
2022-09-09 13:29:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     description="Reference implementation of LassoNet",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    url="https://github.com/ilemhadri/lassonet",
+    url="https://github.com/lasso-net/lassonet",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -31,5 +31,5 @@ setup(
         "tqdm",
     ],
     tests_require=["pytest"],
-    python_requires=">=3.6.5",
+    python_requires=">=3.8",
 )
