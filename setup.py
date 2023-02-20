f0d16d8b63982df20667e2e91912d2fad3f616cd
Louis Abraham <louis.abraham@yahoo.fr>
2023-02-20 10:31:54 +0100

    v0.0.14

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name="lassonet",
-    version="0.0.13",
+    version="0.0.14",
     author="Louis Abraham, Ismael Lemhadri",
     author_email="louis.abraham@yahoo.fr, lemhadri@stanford.edu",
     license="MIT",
